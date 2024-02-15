1aa07d0482d10b619e94de5cd8ac6eafa1224968
Sharan Thakur <sharandt19@live.com>
2024-02-15 23:05:00 +0530

    refactors code

--- image_diffuser_gradio_app.py
+++ image_diffuser_gradio_app.py
@@ -13,6 +13,7 @@ model_choices = [
     "dataautogpt3/ProteusV0.3"
 ]
 
+
 def get_pipeline(model_name: str):
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -20,13 +21,15 @@ def get_pipeline(model_name: str):
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
-        print ("CUDA & MPS devices not found.")
+        print("CUDA & MPS devices not found.")
 
     print("Testing torch device")
-    torch.ones(2, device=device) 
+    torch.ones(2, device=device)
 
     if model_name == model_choices[0]:
-        pipeline = DiffusionPipeline.from_pretrained("stablediffusionapi/juggernaut-xl-v5").to(device)
+        pipeline = DiffusionPipeline.from_pretrained(
+            "stablediffusionapi/juggernaut-xl-v5",
+        ).to(device)
         pipeline.load_lora_weights("ehristoforu/dalle-3-xl")
     else:
         # Load VAE component
@@ -36,7 +39,7 @@ def get_pipeline(model_name: str):
         )
         # Configure the pipeline
         pipeline = StableDiffusionXLPipeline.from_pretrained(
-            model_name, 
+            model_name,
             vae=vae,
             torch_dtype=torch.float16
         )
@@ -44,21 +47,22 @@ def get_pipeline(model_name: str):
         pipeline = pipeline.to(device)
     return pipeline
 
+
 def infer(
-    prompt: str,
-    negative_prompt: str,
-    cfg_scale: float,
-    guidance_scale: float,
-    num_inference_steps: int,
-    images_per_prompt: int,
-    width: int,
-    height: int,
-    model_choice: str
+        prompt: str,
+        negative_prompt: str,
+        cfg_scale: float,
+        guidance_scale: float,
+        num_inference_steps: int,
+        images_per_prompt: int,
+        width: int,
+        height: int,
+        model_choice: str
 ):
     pipeline = get_pipeline(model_choice)
     output = pipeline(
-        prompt, 
-        negative_prompt=negative_prompt, 
+        prompt,
+        negative_prompt=negative_prompt,
         width=width,
         height=height,
         num_images_per_prompt=images_per_prompt,
@@ -68,14 +72,17 @@ def infer(
     )
     return output.images
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     demo = gr.Interface(
         fn=infer,
-        allow_flagging=False,
+        allow_flagging='never',
         inputs=[
             gr.TextArea("", label="Image Prompt"),
             gr.Text(
-                "nsfw, bad quality, bad anatomy, worst quality, low quality, low resolutions, extra fingers, blur, blurry, ugly, wrongs proportions, watermark, image artifacts, lowres, ugly, jpeg artifacts, deformed, noisy image",
+                "nsfw, bad quality, bad anatomy, worst quality, low quality, low resolutions, extra fingers, blur, "
+                "blurry, ugly, wrongs proportions, watermark, image artifacts, lowres, ugly, jpeg artifacts, "
+                "deformed, noisy image",
                 label="Negative Prompts"
             ),
             gr.Slider(6, 9, label="CFG Scale"),
@@ -93,5 +100,5 @@ if __name__=='__main__':
         outputs=gr.Gallery(label="Output Images")
     )
 
-    demo.queue(concurrency_count=2)
+    demo.queue()
     demo.launch(share=True)
